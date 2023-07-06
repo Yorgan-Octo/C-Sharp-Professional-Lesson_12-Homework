a28d1fe3ac6f8dfebe86ce0705aba7f98c5176c0
Artem Priadchenko <yorgan.info@gmail.com>
2023-07-06 23:24:50 +0300

    Добавьте файлы проекта.

---
+++ Task_3/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Task_3
+{
+    internal class Program
+    {
+
+        static Mutex mutex = new Mutex(false, "NDB");
+
+        public static void Method1(Object state)
+        {
+            mutex.WaitOne();
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Method1 - " + i);
+                Thread.Sleep(500);
+            }
+
+            Thread.Sleep(500);
+            Console.WriteLine();
+            mutex.ReleaseMutex();
+        }
+
+        public static void Method2(Object state)
+        {
+            mutex.WaitOne();
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Method2 - " + i);
+                Thread.Sleep(500);
+            }
+            Thread.Sleep(500);
+            Console.WriteLine();
+            mutex.ReleaseMutex();
+        }
+
+        static void Main()
+        {
+            Thread.Sleep(800);
+
+            ThreadPool.QueueUserWorkItem(Method1);
+            ThreadPool.QueueUserWorkItem(Method2);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task_5/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Task_5
+{
+    internal class Program
+    {
+        static string logFilePath = "logfile.log";
+        static object locOb = new object();
+
+        static Semaphore semaphore = new Semaphore(2, 6, "Sim345788");
+
+
+        public static void LogMenejaError(object ob)
+        {
+            Thread.Sleep(1000);
+            semaphore.WaitOne();
+
+            try
+            {
+                throw new Exception("Тут что то пошло не так: " + DateTime.Now + " ///  ");
+            }
+            catch (Exception ex)
+            {
+                lock (locOb)
+                {
+                    File.AppendAllText(logFilePath, ex.Message);
+                }
+
+                Console.WriteLine(ex.Message);
+                Thread.Sleep(2000);
+                Console.WriteLine("Записанно в logfile.log");
+            }
+
+            semaphore.Release();
+
+        }
+
+
+        static void Main()
+        {
+
+            ThreadPool.QueueUserWorkItem(LogMenejaError);
+            ThreadPool.QueueUserWorkItem(LogMenejaError);
+            ThreadPool.QueueUserWorkItem(LogMenejaError);
+            ThreadPool.QueueUserWorkItem(LogMenejaError);
+            ThreadPool.QueueUserWorkItem(LogMenejaError);
+
+            Console.ReadKey();
+
+        }
+    }
+}
